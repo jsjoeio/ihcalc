bb3c4f0546fa78a742d8c8873ccc3949d2b75726
Joe Previte <jjprevite@gmail.com>
2023-09-27 21:14:41 +0000

    chore: upgrade to astro v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
@@ -9,7 +8,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [tailwind(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), mdx(), sitemap(), react()]
+  integrations: [tailwind(), mdx(), sitemap(), react()]
 });

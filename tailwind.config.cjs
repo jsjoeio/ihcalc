0c741081a1a15ed0fe81d486e662ea9a0d1080d6
Joe Previte <jjprevite@gmail.com>
2023-10-12 04:39:01 +0000

    refactor: update colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,13 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        'white': '#FFFCF0',
+        'black': '#100F0F',
+        'slate': {
+          500: '#6F6E69',
+        },
+      },
       fontFamily: {
         sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
       },

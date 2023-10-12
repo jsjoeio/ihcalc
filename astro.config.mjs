7e047156236c83e44231eccd9e9a387979949e11
Joe Previte <jjprevite@gmail.com>
2023-10-12 04:44:11 +0000

    chore: update SEO info

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://ihcalc.vercel.app/",
   integrations: [tailwind(), mdx(), sitemap(), react()]
 });

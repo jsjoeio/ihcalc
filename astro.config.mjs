fd0c6e2462391bbbdb87fc467683fc0771327447
Joe Previte <jjprevite@gmail.com>
2023-10-25 15:07:24 +0000

    refactor: switch to server render

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import nodejs from "@astrojs/node"
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: nodejs({
+    mode: "middleware"
+  }),
   site: "https://www.howmuchtomake.org/",
   integrations: [tailwind(), mdx(), sitemap(), react()]
 });

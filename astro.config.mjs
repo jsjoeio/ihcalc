f7cfdd3badb0a5c1380b1739deba57cdd2a0127a
Joe Previte <jjprevite@gmail.com>
2023-10-25 15:11:21 +0000

    fix: use vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,14 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import nodejs from "@astrojs/node"
+import vercel from '@astrojs/vercel/serverless';
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: nodejs({
-    mode: "middleware"
-  }),
+  adapter: vercel(),
   site: "https://www.howmuchtomake.org/",
   integrations: [tailwind(), mdx(), sitemap(), react()]
 });

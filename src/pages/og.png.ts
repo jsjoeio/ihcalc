1a2547b6407487157eabf3c07bb1099a17db37c9
Tad Scritchfield <tadscritch@gmail.com>
2023-11-01 10:56:39 -0500

    fix fonts in og image

--- src/pages/og.png.ts
+++ src/pages/og.png.ts
@@ -7,17 +7,34 @@ import type { APIRoute } from "astro";
 export const get: APIRoute = async function get({ url, request }) {
   const [totalMoney, numOfMonths, offeringPrice] = url.searchParams
     .get("v")
-    ?.split("-").map(v => parseInt(v)) || [50000, 3, 50];
-  const interFont = await fs.readFile("./public/fonts/Inter-Regular.ttf");
+    ?.split("-")
+    .map((v) => parseInt(v)) || [50000, 3, 50];
+  const interRegular = await fs.readFile("./public/fonts/Inter-Regular.ttf");
+  const interExtraBold = await fs.readFile(
+    "./public/fonts/Inter-ExtraBold.ttf"
+  );
+  const interLight = await fs.readFile("./public/fonts/Inter-Light.ttf");
   const options = {
     width: 1200,
     height: 630,
     fonts: [
       {
         name: "Inter",
-        data: interFont,
-        weight: 500,
-        style: "normal"
+        data: interRegular,
+        weight: 400,
+        style: "normal",
+      },
+      {
+        name: "Inter",
+        data: interExtraBold,
+        weight: 800,
+        style: "normal",
+      },
+      {
+        name: "Inter",
+        data: interLight,
+        weight: 300,
+        style: "normal",
       },
     ],
   };
@@ -38,18 +55,19 @@ export const get: APIRoute = async function get({ url, request }) {
     style="height: 100%; width: 100%; display: flex; flex-direction: column; align-items: center; justify-content: center; background-color: rgb(255, 252, 240); ">
     <div
       style="display: flex; flex-direction: row; align-items: center; justify-content: space-around; width: 80%;">
-      <img src="https://res.cloudinary.com/jsjoeio/image/upload/v1698812392/logopng_wvhtza.png" width="300" height="300" />
+      <img
+        src="https://res.cloudinary.com/jsjoeio/image/upload/v1698812392/logopng_wvhtza.png"
+        width="300"
+        height="300" />
       <div
-        style="display: flex; flex-direction: column; font-size: 64px; font-weight: 800; font-family: 'Inter'">
-        <div>
-          Make
-          ${formatter.format(totalMoney)}
-        </div>
+        style="display: flex; flex-direction: column; font-size: 64px; font-weight: 800; font-family: Noto;">
+        <div>Make ${formatter.format(totalMoney)}</div>
         <div>in ${numOfMonths.toLocaleString()} months selling</div>
         <div>a ${formatter.format(offeringPrice)} product.</div>
       </div>
     </div>
-    <div style="font-size: 28px; font-weight: 300; margin-top: 24px; align-self: flex-end; margin-right: 60px;">
+    <div
+      style="font-size: 28px; font-weight: 300; margin-top: 24px; align-self: flex-end; margin-right: 60px;">
       howmuchtomake.org
     </div>
   </div>`;
